970c98a6509375cd4e7f6b3723c6a53d435a1f44
Chun-da Chen <capitalm.c@gmail.com>
2016-10-24 22:06:33 +0800

    Add user `mqtt` and set permissions.

--- mq/configure.sh
+++ mq/configure.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-# `graylog []`
-rabbitmqctl list_users | grep '^graylog\s' >/dev/null \
-  || rabbitmqctl add_user graylog graylog
+add_user() {
+  user="$1"
+  pass="$2"
 
-# `/       log-messages    log-messages    log-messages`
-rabbitmqctl list_user_permissions graylog \
-  | grep '^/\slog-messages\slog-messages\slog-messages' >/dev/null \
-  || rabbitmqctl set_permissions -p / graylog log-messages log-messages log-messages
+  # `graylog []`
+  rabbitmqctl list_users | grep "^$user\\s" >/dev/null \
+    || rabbitmqctl add_user "$user" "$pass"
+
+  # `/       log-messages    log-messages    log-messages`
+  rabbitmqctl list_user_permissions "$user" \
+    | grep "^/\s$3\s$4\s$5" >/dev/null \
+    || rabbitmqctl set_permissions -p / "$user" "$3" "$4" "$5"
+}
+
+add_user graylog graylog log-messages log-messages log-messages
+add_user mqtt mqtt 'mqtt-subscription-paho/.*' 'amq.topic|log\..*' ''
